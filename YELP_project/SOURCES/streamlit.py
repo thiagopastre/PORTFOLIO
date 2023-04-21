0e76ca46ba742f2ea4fe2d8b27286223e5182366
Thiago <thiago.pastre@hotmail.com>
2023-04-21 20:39:24 -0300

    Streamlit tests

--- YELP_project/SOURCES/streamlit.py
+++ YELP_project/SOURCES/streamlit.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 
-st.write('Hello world!')
+
+st.title('EDA Yelp Project')
+
+# Personalizando o SideBar
+st.sidebar.title('Menu')
+
+#Setup do upload do arquivo
+arquivo=st.sidebar.file_uploader(
+    label='Selecione o arquivo csv',
+    type=['csv']
+)
