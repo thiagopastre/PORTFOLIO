9f834176bd4a9c65a096ed9ff154cbd0ba092042
Thiago <thiago.pastre@hotmail.com>
2023-04-22 16:45:17 -0300

    Streamlit tests

--- YELP_project/SOURCES/streamlit.py
+++ YELP_project/SOURCES/streamlit.py
@@ -12,6 +12,10 @@ st.sidebar.title('Menu')
 
 #Setup do upload do arquivo
 arquivo=st.sidebar.file_uploader(
-    label='Selecione o arquivo csv',
+    label='Selecione o arquivo CSV',
     type=['csv']
 )
+
+if arquivo is not None:
+    business = pd.read_csv(arquivo).drop('Unnamed: 0', axis=1)
+    print(business.head(3))
