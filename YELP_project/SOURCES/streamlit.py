2ff97738eba74162779c7b9ebea00cbfa09b1be6
Thiago Pastre <thiago.pastre@hotmail.com>
2023-04-23 13:15:21 -0300

    streamlit tests

--- YELP_project/SOURCES/streamlit.py
+++ YELP_project/SOURCES/streamlit.py
@@ -8,36 +8,71 @@ import plotly.express as px
 st.title('EDA Yelp Project')
 
 # Personalizando o SideBar
-st.sidebar.title('Menu')
+# st.sidebar.title('Menu')
 
 #Setup do upload do arquivo
 file=st.sidebar.file_uploader(
-    label='Selecione o arquivo PKL',
+    label='Selecione o arquivo business_EDITED.pkl',
     type=['pkl']
 )
 
 if file is not None:
     business = pd.read_pickle(file)
-    st.write(business.head())
 
+    graphs = ['Distribuição do target (stars)',
+            'Quantidade de negócios cadastrados por Estado (barplot)',
+            'Quantidade de negócios cadastrados por Estado (map)'
+            ]
 
-    stars = business['stars'].agg('value_counts')
-
-    fig, ax = plt.subplots(1,2, figsize=[12,4])
-
-    sns.boxplot(ax=ax[0],
-                data=business,
-                x='stars',
-                palette='PuBu'
+    option = st.selectbox(
+            "Qual gráfico gostaria de analisar?",
+            graphs,
+            label_visibility=st.session_state.visibility,
+            disabled=st.session_state.disabled
             )
 
-    sns.barplot(ax=ax[1],
-                x=stars.index,
-                y=stars,
-                palette='PuBu'
-            )
+    
+    if option == 'Distribuição do target (stars)':
+        stars = business['stars'].agg('value_counts')
 
-    fig.suptitle('Distribuição da feature "stars"')
-    ax[1].set_xlabel('Stars')
-    ax[1].set_ylabel('Quantidade de avaliações')
-    st.pyplot(fig)
+        fig, ax = plt.subplots(1,2, figsize=[12,4])
+
+        sns.boxplot(ax=ax[0],
+                    data=business,
+                    x='stars',
+                    palette='PuBu'
+                )
+
+        sns.barplot(ax=ax[1],
+                    x=stars.index,
+                    y=stars,
+                    palette='PuBu'
+                )
+
+        fig.suptitle('Distribuição da feature "stars"')
+        ax[1].set_xlabel('Stars')
+        ax[1].set_ylabel('Quantidade de avaliações')
+        st.pyplot(fig)
+    
+    elif option == 'Quantidade de negócios cadastrados por Estado (barplot)':
+
+        # Criando um Dataframe contendo a quantidade TOTAL de negócios (abertos e fechados) agrupados por Estado
+        df_total_business = business[['state','is_open']].groupby('state').sum()
+        df_total_business['total'] = business[['state','is_open']].groupby('state').count()
+        df_total_business = df_total_business.sort_values(by='total', ascending=False).reset_index()
+
+        fig, ax = plt.subplots(figsize=[15,5])
+
+        ax = sns.barplot(x=df_total_business['state'],
+                        y=df_total_business['total'],
+                        palette='PuBu_r',
+                        order=df_total_business['state']
+                        )
+        for i in ax.containers:
+            ax.bar_label(i,)
+
+        ax.set(xlabel='Estados',
+            ylabel='Quantidade',
+            title='Quantidade de negócios cadastrados por Estado'
+            )
+        st.pyplot(fig)
