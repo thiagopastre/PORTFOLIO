12a2e735711057d5e5dac281705b1808719a9d48
Thiago Pastre <thiago.pastre@hotmail.com>
2023-04-23 12:56:00 -0300

    streamlit tests

--- YELP_project/SOURCES/streamlit.py
+++ YELP_project/SOURCES/streamlit.py
@@ -19,3 +19,25 @@ file=st.sidebar.file_uploader(
 if file is not None:
     business = pd.read_pickle(file)
     st.write(business.head())
+
+
+    stars = business['stars'].agg('value_counts')
+
+    fig, ax = plt.subplots(1,2, figsize=[12,4])
+
+    sns.boxplot(ax=ax[0],
+                data=business,
+                x='stars',
+                palette='PuBu'
+            )
+
+    sns.barplot(ax=ax[1],
+                x=stars.index,
+                y=stars,
+                palette='PuBu'
+            )
+
+    fig.suptitle('Distribuição da feature "stars"')
+    ax[1].set_xlabel('Stars')
+    ax[1].set_ylabel('Quantidade de avaliações')
+    st.pyplot(fig)
