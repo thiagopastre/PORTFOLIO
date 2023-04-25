6703c1a8a7d240a36160eedf927cee94fdb1eb08
Thiago Pastre <thiago.pastre@hotmail.com>
2023-04-25 07:57:48 -0300

    Streamlit EDA

--- YELP_project/SOURCES/streamlit.py
+++ YELP_project/SOURCES/streamlit.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import requests
+from io import BytesIO
 
 @st.cache(allow_output_mutation=True)
 def load_file(url):
